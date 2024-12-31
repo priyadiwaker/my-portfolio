c9fb8ee8ad589d9189e810c551298a164c08e168
priyadiwaker <98397008+priyadiwaker@users.noreply.github.com>
2024-12-31 10:08:25 -0800

    Add files via upload

---
+++ gsap.js
@@ -0,0 +1,6 @@
+gsap.from(".hero-img", {
+    y: "25rem",
+    rotation: 60, // Correct property for rotation
+    duration: 2,   // Add a duration (in seconds) for the animation
+    ease: "power3.out" // Optional easing for a smooth effect
+});
---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    const menuIcon = document.querySelector('.menu-icon');
+    const menu = document.querySelector('.menu');
+    menuIcon.classList.toggle('open');
+    menu.classList.toggle('open');
+}
+
+
+
